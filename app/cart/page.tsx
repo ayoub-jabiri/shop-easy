7955359fc1dcd5d0b033ec81159ebcd3ccbb13c7
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-06-19 16:17:17 +0100

    Added a back up message if the cart is empty

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -23,14 +23,21 @@ export default function Cart() {
                     </ul>
                 </div>
                 <h1 className="text-3xl font-bold mb-6">Your Shopping Cart</h1>
-                <div className="grid grid-cols-12 gap-5">
-                    <div className="col-span-12 md:col-span-7">
-                        <CartItems cartItems={cartData.cartItems} />
+                {!cartData.cartItemsCount && (
+                    <div>
+                        <p>The cart is Empty!</p>
                     </div>
-                    <div className="col-span-12 md:col-span-5">
-                        <OrderSummary />
+                )}
+                {cartData.cartItemsCount > 0 && (
+                    <div className="grid grid-cols-12 gap-5">
+                        <div className="col-span-12 md:col-span-7">
+                            <CartItems cartItems={cartData.cartItems} />
+                        </div>
+                        <div className="col-span-12 md:col-span-5">
+                            <OrderSummary />
+                        </div>
                     </div>
-                </div>
+                )}
             </div>
         </div>
     );
